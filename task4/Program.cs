35c38656eb80c98b90db8d168fe28f2060d9a78a
Vitaly Kryukov <v.b.kryukov@mail.ru>
2022-10-02 20:07:51 +0300

    написали 1-е методы: - заполнение массива; -печать массива

---
+++ task4/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;//получаем длинну массива
+    int index = 0;
+    while(index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);//в колекцию положить случайные числа из диапозона 1,10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; //задали массив в котором будет 10 элементов
+
+FillArray(array);//метод, который заполнил нам массив
+PrintArray(array);//метод, который распечатал нам массив
