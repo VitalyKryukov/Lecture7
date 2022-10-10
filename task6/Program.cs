b998d8d6be13f0fdc06398956cfbbc0cdd07bc78
Vitaly Kryukov <v.b.kryukov@mail.ru>
2022-10-10 21:30:16 +0300

    Решили задачи к 3 уроку

---
+++ task6/Program.cs
@@ -0,0 +1,75 @@
+﻿Console.Clear();
+// Вид 1. Ничего не возвращают и не принимают.
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+//Method1(); //Так вызывается метод
+
+//Вид 2. Ничего не возвращают и принимают.
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
+
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method2(msg: "Текст сообщения", count: 4);
+//Method2(count: 4, msg: "Новый текст");
+
+//Вид 3. Возвращают но ничего не принимают.
+int Method3() //Если метод что-либо возвращает обязательно указывается тип данных.
+{
+    return DateTime.Now.Year; //Если возвращает, обязательно использовать return.
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+//Вид 4. Принимают и возвращают.
+//Вариант с while
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res =  Method4(10, "z");
+// Console.WriteLine(res);
+
+//Вариант с for.
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for( int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res =  Method4(10, "z");
+//Console.WriteLine(res);
+
+//Цикл в цикле.
+//Вывод таблицы умножения на экран.
+
+for(int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
---
+++ task8ArrayMax/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+// Сортировка алгоритма (от макс к мин).
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    // int count = array.Length;
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i+1; j < array.Length; j++) // ищет минимальный элемент.
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i]; //производится замена
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
